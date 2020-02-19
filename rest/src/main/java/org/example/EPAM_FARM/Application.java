5725bcb0a86cca46c77059cb517ed3951451c8d6
Uladzislau Biadrytski <uladzislau_biadrytski@epam.com>
2020-02-19 18:59:14 +0300

    changed liquibase changesets format to 'xml'

--- rest/src/main/java/org/example/EPAM_FARM/Application.java
+++ rest/src/main/java/org/example/EPAM_FARM/Application.java
@@ -3,8 +3,6 @@ package org.example.EPAM_FARM;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
--- rest/src/main/resources/db.changelog/sql/V4__insert_into_employees.sql
+++ rest/src/main/resources/db.changelog/sql/V4__insert_into_employees.sql
@@ -1,3 +1,3 @@
 INSERT INTO employees
-    (id, name, date_of_birth, salary, department_id)
+    (id, name, birthday, salary, department_id)
     VALUES (1, 'Mike', '1992-12-24', 150012.12, 1);
