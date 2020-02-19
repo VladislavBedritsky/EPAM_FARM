0cd4b4cd931a6bf349505ecf680bff1c158d93b7
Uladzislau Biadrytski <uladzislau_biadrytski@epam.com>
2020-02-19 19:20:04 +0300

    change app version to 1.7

--- rest/src/main/java/org/example/EPAM_FARM/Application.java
+++ rest/src/main/java/org/example/EPAM_FARM/Application.java
@@ -3,19 +3,21 @@ package org.example.EPAM_FARM;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"org.example"})
 @ComponentScan(basePackages = {"org.example"})
-public class Application  {
-// extends SpringBootServletInitializer
+public class Application extends SpringBootServletInitializer {
+//
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
 
-//        @Override
-//        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(Application.class);
-//    }
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 }
