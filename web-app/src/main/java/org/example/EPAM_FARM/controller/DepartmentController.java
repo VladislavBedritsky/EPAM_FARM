6eb03a7b3bf3e5efa66cb7f35101658cb6a1a747
Uladzislau Biadrytski <uladzislau_biadrytski@epam.com>
2020-02-26 13:24:43 +0300

    updated views and added static-css

--- web-app/src/main/java/org/example/EPAM_FARM/controller/DepartmentController.java
+++ web-app/src/main/java/org/example/EPAM_FARM/controller/DepartmentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +20,16 @@ public class DepartmentController {
     public String getDepartments (Model model) {
         model.addAttribute("departments", jdbcStorageService.findAll());
         return "departments";
+    }
+
+    @GetMapping("/{id}")
+    public String getDepartmentView (
+            Model model,
+            @PathVariable Integer id) {
+
+        model.addAttribute("id",id);
+
+        return "department";
     }
 
 
--- web-app/src/main/java/org/example/EPAM_FARM/controller/EmployeeController.java
+++ web-app/src/main/java/org/example/EPAM_FARM/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +19,14 @@ public class EmployeeController {
     public String getEmployees(Model model) {
         model.addAttribute("employees",employeeService.findAll());
         return "employees";
+    }
+
+    @GetMapping("/{id}")
+    public String getEmployeeView(
+            Model model,
+            @PathVariable Integer id
+    ) {
+        model.addAttribute("id",id);
+        return "employee";
     }
 }
