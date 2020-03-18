8eac10bc9df55bdaca7a0ebb4fe5a611f68251e6
Vladislav Bedritsky <vladquinn2010@gmail.com>
2020-03-18 14:24:32 +0300

    update db and an Angular app

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
-import { EmployeeService } from './service/employee.service'
+import { EmployeeService } from './service/employee.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
