cd7c14aa1946261239bfbe438b08cfb3d0d5da4f
Vladislav Bedritsky <vladquinn2010@gmail.com>
2020-06-01 19:35:16 +0300

    mail service is added

--- mail/src/main/java/org/example/epam/service/MailSender.java
+++ mail/src/main/java/org/example/epam/service/MailSender.java
@@ -1,4 +1,20 @@
 package org.example.epam.service;
 
+/**
+ * MailSender Service
+ *
+ * @version 1.01 01 Jun 2020
+ * @author Uladzislau Biadrytski
+ */
 public interface MailSender {
+
+    /**
+     * Send an email.
+     *
+     * @param emailTo Recipient email
+     * @param subject Email topic
+     * @param message Email message
+     */
+    void sendEmail (String emailTo, String subject, String message);
+
 }
--- mail/src/main/java/org/example/epam/service/impl/MailSenderImpl.java
+++ mail/src/main/java/org/example/epam/service/impl/MailSenderImpl.java
@@ -1,4 +1,38 @@
 package org.example.epam.service.impl;
 
-public class MailSenderImpl {
+import org.example.epam.service.MailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+/**
+ * MailSender interface implementation
+ *
+ * @version 1.01 01 Jun 2020
+ * @author Uladzislau Biadrytski
+ */
+@Service
+@PropertySource("classpath:application.properties")
+public class MailSenderImpl implements MailSender {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Override
+    public void sendEmail (String emailTo, String subject, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom(username);
+        mailMessage.setTo(emailTo);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+
+        mailSender.send(mailMessage);
+    }
 }
