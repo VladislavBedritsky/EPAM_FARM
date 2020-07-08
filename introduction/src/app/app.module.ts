c86d492f38ee6a7091ac826de848904d0acade80
Vladislav Bedritsky <vladquinn2010@gmail.com>
2020-07-08 13:27:34 +0300

    add material.module in introduction app

--- introduction/src/app/app.module.ts
+++ introduction/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './module/app-routing.module';
+import { MaterialModule } from './module/material.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
